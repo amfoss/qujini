6d582a70783d36b30f90186f003129df86b2f68d
root <root@11cpu0197L.amritavidya.edu>
2019-05-30 14:35:45 +0530

    add ckeditor

--- qpg/settings.py
+++ qpg/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
