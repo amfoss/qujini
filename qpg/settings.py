a946e09569c86a99f3ead45170420b1e5dd45127
arv-lochanan <arv.lochanan@gmail.com>
2019-05-30 15:22:01 +0530

    added_comma

--- qpg/settings.py
+++ qpg/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'questions'
-    'ckeditor',
+    'questions',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
