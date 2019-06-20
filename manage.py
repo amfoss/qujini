3b574975f8b3fc0127ccaced99ff88d4e6d2718d
harshithpabbati <pabbatiharshith@gmail.com>
2019-06-20 22:31:33 +0530

    Renamed qpg to qujini

--- manage.py
+++ manage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qpg.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qujini.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,7 +13,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
