6c41fabbf4e8a6dbf66234e58d25e77ae8de4ee0
harshithpabbati <pabbatiharshith@gmail.com>
2019-06-20 22:15:47 +0530

    Fix indentation errors

--- questions/models.py
+++ questions/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
+
 class Topic(models.Model):
-	name=models.CharField(max_length=50)
-	parent=models.ForeignKey('self',on_delete=models.CASCADE,null=True, blank=True)
-	
-  def __str__(self):
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+
+def __str__(self):
     return self.name
+
 
 class Type(models.Model):
-  name = models.CharField(max_length = 50)
-  class Meta:
-    verbose_name = "Type"
-    verbose_name_plural = "Types"
+    name = models.CharField(max_length=50)
 
-  def __str__(self):
-    return self.name
+    class Meta:
+        verbose_name = "Type"
+        verbose_name_plural = "Types"
+
+    def __str__(self):
+        return self.name
