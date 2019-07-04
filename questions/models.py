42dea2cfc7f285ed260eb0480b8f22ef9d67ad83
Nishant Pandey <pnishant46@gmail.com>
2019-07-04 11:32:37 +0530

    docs: Question paper model changed

--- questions/models.py
+++ questions/models.py
@@ -1,21 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class questions(models.Model):
+    name= models.CharField(max_length=255)
+    relation= models.ManyToManyField('self')
 
-class Topic(models.Model):
-    name = models.CharField(max_length=50)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-
-
-def __str__(self):
-    return self.name
-
-
-class Type(models.Model):
-    name = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = "Type"
-        verbose_name_plural = "Types"
-
-    def __str__(self):
-        return self.name
+class creator(models.Model):
+     user= models.CharField(max_length=50)
+     designed_by= models.ForeignKey('self', on_delete= models.CASCADE)
