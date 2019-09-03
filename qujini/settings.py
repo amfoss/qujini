2e96fb246311ac54b2354526ffbba9b77e87a8e5
amreshk005 <amreshk005@gmail.com>
2019-09-03 19:50:58 +0530

    Admin UI Change

--- qujini/settings.py
+++ qujini/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'qujini.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["static", "templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
