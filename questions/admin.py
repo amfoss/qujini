68792bd06c9415435407d909340d65454a18a8f0
amreshk005 <amreshk005@gmail.com>
2019-09-03 18:08:25 +0530

    Admin UI Changed

--- questions/admin.py
+++ questions/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Topic)
-admin.site.register(Type)
+
+class SnippetAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/questions/login.html'
+
+
+admin.site.register(Topic, SnippetAdmin)
+admin.site.register(Type, SnippetAdmin)
--- qujini/settings.py
+++ qujini/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'questions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'questions',
     'ckeditor',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'qujini.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["static", "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
