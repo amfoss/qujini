e25422dbc9d863b95a90f7aa201fd0d5a362c0a8
harshithpabbati <pabbatiharshith@gmail.com>
2019-09-04 20:30:09 +0530

    templates: Add admin templates

--- qujini/settings.py
+++ qujini/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'qujini.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
