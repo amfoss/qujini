e1faf4141c842353f69204301d6b754ed3c0de85
Amresh Kumar <35935083+amreshk005@users.noreply.github.com>
2019-09-12 17:11:35 +0530

    Fix-#5 (#29)
    Alignment Fixed
    
    Alignment Fixed
    
    Alignment Fixed
    
    Question Model Added

--- questions/admin.py
+++ questions/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from .models import *
 
 
-class SnippetAdmin(admin.ModelAdmin):
-    change_list_template = 'admin/questions/login.html'
+class OrderAdmin(admin.ModelAdmin):
+    fields = ('author', ('min_mark', 'max_mark', 'difficulty', 'date'),
+              'question_type', 'question', 'topic')
+
+    fields = ('author', ('min_mark', 'max_mark', 'difficulty', 'date'),
+              'topic', 'questionType', 'question',)
+
+    list_display = ('question_trim', 'mark_range',
+                    'difficulty', 'questionType', 'get_topics', 'author')
+
+    search_fields = ('question_trim', 'difficulty',
+                     'questionType', 'get_topics' 'author')
 
 
-admin.site.register(Topic, SnippetAdmin)
-admin.site.register(Type, SnippetAdmin)
+admin.site.register(Topic)
+admin.site.register(Type)
+admin.site.register(Question, OrderAdmin)
--- questions/models.py
+++ questions/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import truncatechars
+from ckeditor.fields import RichTextField
 
 
 class Topic(models.Model):
     name = models.CharField(max_length=50)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-       return self.name
+        return self.name
 
 
 class Type(models.Model):
@@ -20,3 +23,37 @@ class Type(models.Model):
     def __str__(self):
         return self.name
 
+
+class Question(models.Model):
+    VeryEasy = 'Very Easy'
+    Easy = 'Easy'
+    Moderate = 'Moderate'
+    Expert = 'Expert'
+    difficulty_choices = [
+        (VeryEasy, 'VeryEasy'),
+        (Easy, 'Easy'),
+        (Moderate, 'Moderate'),
+        (Expert, 'Expert'),
+    ]
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    min_mark = models.IntegerField()
+    max_mark = models.IntegerField(null=True)
+    difficulty = models.CharField(
+        max_length=20,
+        choices=difficulty_choices,
+        default=VeryEasy
+    )
+    date = models.DateField()
+    questionType = models.ForeignKey(Type, on_delete=models.CASCADE)
+    question = RichTextField(blank=True, null=True)
+    topic = models.ManyToManyField(Topic)
+
+    def mark_range(self):
+        return '{}-{}'.format(self.min_mark, self.max_mark)
+
+    def get_topics(self):
+        return "\n".join([p.name for p in self.topic.all()])
+
+    @property
+    def question_trim(self):
+        return truncatechars(self.question, 100)
